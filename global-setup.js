972c6820da30fc2fe94c7781b3f17202df6015d0
Roman <dftns4@gmail.com>
2026-03-28 00:46:33 +0100

    config updated

--- global-setup.js
+++ global-setup.js
@@ -1,13 +1,13 @@
-import fs from "fs";
-import path from "path";
+// import fs from "fs";
+// import path from "path";
 
-export default async () => {
-  const dirs = ["allure-report", "allure-results"];
+// export default async () => {
+//   const dirs = ["allure-report", "allure-results"];
 
-  for (const dir of dirs) {
-    const fullPath = path.resolve(dir);
-    if (fs.existsSync(fullPath)) {
-      fs.rmSync(fullPath, { recursive: true, force: true });
-    }
-  }
-};
+//   for (const dir of dirs) {
+//     const fullPath = path.resolve(dir);
+//     if (fs.existsSync(fullPath)) {
+//       fs.rmSync(fullPath, { recursive: true, force: true });
+//     }
+//   }
+// };
