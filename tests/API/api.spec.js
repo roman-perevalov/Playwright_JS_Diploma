541eeae10a725b780ed34c06fbb9c30277c184dd
Roman <dftns4@gmail.com>
2026-03-27 23:40:05 +0100

    tags updated

--- tests/API/api.spec.js
+++ tests/API/api.spec.js
@@ -11,66 +11,98 @@ test.describe("API Challenges", () => {
     console.log(`${testinfo.project.use.apiURL}/gui/challenges/${token}`);
   });
 
-  test(`Получить список челленджей (200)`, async ({ api }, testinfo) => {
-    let response = await api.challenges.get(testinfo, token); // отправляем запрос /challenges
+  test(
+    `Получить список челленджей (200)`,
+    {
+      tag: ["@API"],
+    },
+    async ({ api }, testinfo) => {
+      let response = await api.challenges.get(testinfo, token); // отправляем запрос /challenges
 
-    expect(response.status).toBe(200);
-    expect(response.body.challenges.length).toBe(59);
-  });
+      expect(response.status).toBe(200);
+      expect(response.body.challenges.length).toBe(59);
+    },
+  );
 
-  test(`Получить список ToDo's (200)`, async ({ api }, testinfo) => {
-    let response = await api.todos.get(testinfo, token); // отправляем запрос /todos
+  test(
+    `Получить список ToDo's (200)`,
+    {
+      tag: ["@API"],
+    },
+    async ({ api }, testinfo) => {
+      let response = await api.todos.get(testinfo, token); // отправляем запрос /todos
 
-    expect(response.status).toBe(200);
-    expect(response.body.todos.length).toBe(10); // прореряем, что пришло 10 объектов ToDo
-  });
+      expect(response.status).toBe(200);
+      expect(response.body.todos.length).toBe(10); // прореряем, что пришло 10 объектов ToDo
+    },
+  );
 
-  test(`Сделать запрос к /todo (404)`, async ({ api }, testinfo) => {
-    let response = await api.todos.getTodo(testinfo, token); // отправляем запрос /todo
+  test(
+    `Сделать запрос к /todo (404)`,
+    {
+      tag: ["@API"],
+    },
+    async ({ api }, testinfo) => {
+      let response = await api.todos.getTodo(testinfo, token); // отправляем запрос /todo
 
-    expect(response.status).toBe(404); // проверяем статус-ответ респонза
-  });
+      expect(response.status).toBe(404); // проверяем статус-ответ респонза
+    },
+  );
 
-  test(`Запросить ToDo по существующему id (200)`, async ({
-    api,
-  }, testinfo) => {
-    let toDoId = 7; // id существующей задачи в ToDo
-    let response = await api.todos.getTodoById(testinfo, token, toDoId); // отправляем запрос /todos c id таски
+  test(
+    `Запросить ToDo по существующему id (200)`,
+    {
+      tag: ["@API"],
+    },
+    async ({ api }, testinfo) => {
+      let toDoId = 7; // id существующей задачи в ToDo
+      let response = await api.todos.getTodoById(testinfo, token, toDoId); // отправляем запрос /todos c id таски
 
-    expect(response.status).toBe(200); // проверяем статус-ответ
-    expect(response.body.todos[0].id).toBe(toDoId); // проверяем, что в ответе пришел запрашиваемый id
-  });
+      expect(response.status).toBe(200); // проверяем статус-ответ
+      expect(response.body.todos[0].id).toBe(toDoId); // проверяем, что в ответе пришел запрашиваемый id
+    },
+  );
 
-  test(`Запросить ToDo по несуществующему id (404)`, async ({
-    api,
-  }, testinfo) => {
-    let toDoId = 54; // id существующей задачи в ToDo
-    let response = await api.todos.getTodoById(testinfo, token, toDoId); // отправляем запрос /todos c id таски
+  test(
+    `Запросить ToDo по несуществующему id (404)`,
+    {
+      tag: ["@API"],
+    },
+    async ({ api }, testinfo) => {
+      let toDoId = 54; // id существующей задачи в ToDo
+      let response = await api.todos.getTodoById(testinfo, token, toDoId); // отправляем запрос /todos c id таски
 
-    expect(response.status).toBe(404); // проверяем статус-ответ
-  });
+      expect(response.status).toBe(404); // проверяем статус-ответ
+    },
+  );
 
-  test(`Создать ToDo (201)`, async ({ api }, testinfo) => {
-    const newTaskTitle = "Created a task";
-    const newTaskStatus = false;
-    const newTaskDescription = "New task for Test project is created";
+  test(
+    `Создать ToDo (201)`,
+    {
+      tag: ["@API"],
+    },
+    async ({ api }, testinfo) => {
+      const newTaskTitle = "Created a task";
+      const newTaskStatus = false;
+      const newTaskDescription = "New task for Test project is created";
 
-    let response = await api.todos.post(
-      testinfo,
-      token,
-      newTaskTitle,
-      newTaskStatus,
-      newTaskDescription,
-    ); // отправляем запрос /todos и передаем в него информацию по создаваемой таске
+      let response = await api.todos.post(
+        testinfo,
+        token,
+        newTaskTitle,
+        newTaskStatus,
+        newTaskDescription,
+      ); // отправляем запрос /todos и передаем в него информацию по создаваемой таске
 
-    let newResponse = await api.todos.getTodoById(
-      testinfo,
-      token,
-      response.body.id,
-    ); // отправляем запрос /todo с запросом таски по id созданной таски
+      let newResponse = await api.todos.getTodoById(
+        testinfo,
+        token,
+        response.body.id,
+      ); // отправляем запрос /todo с запросом таски по id созданной таски
 
-    expect(newResponse.body.todos[0].title).toBe(newTaskTitle); // проверяем, что заголовок таски соответствует заголовку при создании
-    expect(newResponse.body.todos[0].doneStatus).toBe(newTaskStatus); // проверяем, что статус таски соответствует статусу при создании
-    expect(newResponse.body.todos[0].description).toBe(newTaskDescription); // проверяем, что описание таски соответствует описанию при создании
-  });
+      expect(newResponse.body.todos[0].title).toBe(newTaskTitle); // проверяем, что заголовок таски соответствует заголовку при создании
+      expect(newResponse.body.todos[0].doneStatus).toBe(newTaskStatus); // проверяем, что статус таски соответствует статусу при создании
+      expect(newResponse.body.todos[0].description).toBe(newTaskDescription); // проверяем, что описание таски соответствует описанию при создании
+    },
+  );
 });
--- tests/UI/ui.spec.js
+++ tests/UI/ui.spec.js
@@ -3,90 +3,124 @@ import { test } from "../../src/helpers/fixtures/ui.fixtures";
 import { UserBuilder } from "../../src/helpers/builders/user.builder";
 
 test.describe("Набор функциональных UI-тестов", () => {
-  test("Изменить контактную информацию ", async ({ app, registeredUser }) => {
-    const newUserinfo = new UserBuilder()
-      .addName()
-      .addLastName()
-      .addEmail()
-      .addPhone();
+  test(
+    "Изменить контактную информацию ",
+    {
+      tag: ["@UI"],
+    },
+    async ({ app, registeredUser }) => {
+      const newUserinfo = new UserBuilder()
+        .addName()
+        .addLastName()
+        .addEmail()
+        .addPhone();
 
-    await app.accountPage.goToChangeContactInformation();
+      await app.accountPage.goToChangeContactInformation();
 
-    await app.accountPage.editContactInforfation(
-      newUserinfo.name,
-      newUserinfo.lastName,
-      newUserinfo.email,
-      newUserinfo.phone,
-    );
+      await app.accountPage.editContactInforfation(
+        newUserinfo.name,
+        newUserinfo.lastName,
+        newUserinfo.email,
+        newUserinfo.phone,
+      );
 
-    await app.accountPage.goToChangeContactInformation();
+      await app.accountPage.goToChangeContactInformation();
 
-    await expect(await app.accountPage.getName()).toHaveValue(newUserinfo.name);
-    await expect(await app.accountPage.getLastname()).toHaveValue(
-      newUserinfo.lastName,
-    );
-    await expect(await app.accountPage.getEmail()).toHaveValue(
-      newUserinfo.email,
-    );
-    await expect(await app.accountPage.getPhone()).toHaveValue(
-      newUserinfo.phone,
-    );
-  });
+      await expect(await app.accountPage.getName()).toHaveValue(
+        newUserinfo.name,
+      );
+      await expect(await app.accountPage.getLastname()).toHaveValue(
+        newUserinfo.lastName,
+      );
+      await expect(await app.accountPage.getEmail()).toHaveValue(
+        newUserinfo.email,
+      );
+      await expect(await app.accountPage.getPhone()).toHaveValue(
+        newUserinfo.phone,
+      );
+    },
+  );
 
-  test("Купить iMac", async ({ app, registeredUser }) => {
-    await app.mainPage.goToiMacs();
+  test(
+    "Купить iMac",
+    {
+      tag: ["@UI"],
+    },
+    async ({ app, registeredUser }) => {
+      await app.mainPage.goToiMacs();
 
-    await app.productsPage.addToCart();
-    await app.productsPage.placeAnOrder(
-      "TestName",
-      "TestLastName",
-      "Test Adress 13",
-      "Test City",
-    );
+      await app.productsPage.addToCart();
+      await app.productsPage.placeAnOrder(
+        "TestName",
+        "TestLastName",
+        "Test Adress 13",
+        "Test City",
+      );
 
-    await expect(app.productsPage.getContentLocator()).toContainText(
-      "Ваш заказ принят!",
-    );
-  });
+      await expect(app.productsPage.getContentLocator()).toContainText(
+        "Ваш заказ принят!",
+      );
+    },
+  );
 
-  test("Добавить iMac в «Избранное»", async ({ app, registeredUser }) => {
-    await app.mainPage.goToiMacs();
+  test(
+    "Добавить iMac в «Избранное»",
+    {
+      tag: ["@UI"],
+    },
+    async ({ app, registeredUser }) => {
+      await app.mainPage.goToiMacs();
 
-    const productName = await app.productsPage.getProductName(0);
+      const productName = await app.productsPage.getProductName(0);
 
-    await app.productsPage.addToFavotire(0);
-    await app.productsPage.goToFavorite();
+      await app.productsPage.addToFavotire(0);
+      await app.productsPage.goToFavorite();
 
-    await expect(app.productsPage.getTbodyLocator()).toContainText(productName);
-  });
+      await expect(app.productsPage.getTbodyLocator()).toContainText(
+        productName,
+      );
+    },
+  );
 
-  test("Сравнить товары", async ({ app, registeredUser }) => {
-    await app.mainPage.goToMp3Players();
+  test(
+    "Сравнить товары",
+    {
+      tag: ["@UI"],
+    },
+    async ({ app, registeredUser }) => {
+      await app.mainPage.goToMp3Players();
 
-    const firstItemName = await app.productsPage.getProductName(0);
-    const secondItemName = await app.productsPage.getProductName(1);
+      const firstItemName = await app.productsPage.getProductName(0);
+      const secondItemName = await app.productsPage.getProductName(1);
 
-    await app.productsPage.addToComparisonList(0);
-    await app.productsPage.addToComparisonList(1);
+      await app.productsPage.addToComparisonList(0);
+      await app.productsPage.addToComparisonList(1);
 
-    await app.productsPage.goToComparisonList();
+      await app.productsPage.goToComparisonList();
 
-    const firstComparisonName =
-      await app.productsPage.getComparisonProductName(1);
-    const secondComparisonName =
-      await app.productsPage.getComparisonProductName(2);
+      const firstComparisonName =
+        await app.productsPage.getComparisonProductName(1);
+      const secondComparisonName =
+        await app.productsPage.getComparisonProductName(2);
 
-    expect(firstComparisonName).toBe(firstItemName);
-    expect(secondComparisonName).toBe(secondItemName);
-  });
+      expect(firstComparisonName).toBe(firstItemName);
+      expect(secondComparisonName).toBe(secondItemName);
+    },
+  );
 
-  test("Поиск товара iMac", async ({ app, registeredUser }) => {
-    const product = "Canon EOS 5D";
+  test(
+    "Поиск товара iMac",
+    {
+      tag: ["@UI"],
+    },
+    async ({ app, registeredUser }) => {
+      const product = "Canon EOS 5D";
 
-    await app.mainPage.searchProduct(product);
+      await app.mainPage.searchProduct(product);
 
-    const searchingProductName = await app.productsPage.getProductName(0);
+      const searchingProductName = await app.productsPage.getProductName(0);
 
-    expect(searchingProductName).toBe(product);
-  });
+      expect(searchingProductName).toBe(product);
+    },
+  );
 });
